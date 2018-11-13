446841e1542d26f4524788e830cd2394db3b3be9
Trizzlenova <tdswayzee@gmail.com>
2018-11-13 13:27:23 -0800

    added comments page

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
-app.get("/campgrounds", function(req, res){
+app.get('/campgrounds', function(req, res){
     Campground.find({}, function(err, allCampgrounds){
        if(err){
            console.log(err);
        } else {
-          res.render("index",{campgrounds:allCampgrounds});
+          res.render('campgrounds/index',{campgrounds:allCampgrounds});
        }
     });
 });
@@ -33,13 +33,13 @@ app.post('/campgrounds', function(req, res){
   Campground.create(newCampground, function(err, newlyCreated){
     if(err){console.log(err)}
     else {
-      res.redirect('/campgrounds')
+      res.redirect('campgrounds')
     }
   })
 })
 
 app.get('/campgrounds/new', function(req, res) {
-  res.render('new.ejs')
+  res.render('campgrounds/new')
 })
 
 app.get('/campgrounds/:id', function(req, res) {
@@ -47,7 +47,36 @@ app.get('/campgrounds/:id', function(req, res) {
     if(err){console.log(err)}
     else {
       console.log(foundCampground)
-      res.render('show', {campground:foundCampground})
+      res.render('campgrounds/show', {campground:foundCampground})
+    }
+  })
+})
+
+// COMMENT ROUTES
+
+app.get('/campgrounds/:id/comments/new', function(req, res) {
+  Campground.findById(req.params.id, function(err, campground) {
+    if(err){console.log(err)}
+    else {
+    res.render('comments/new', {campground: campground})
+    }
+  })
+})
+
+app.post('/campgrounds/:id/comments', function(req, res) {
+  Campground.findById(req.params.id, function(err, campground) {
+    if(err){
+      console.log(err)
+      res.redirect('/campgrounds')
+    } else {
+      Comment.create(req.body.comment, function(err, comment) {
+        if(err){console.log(err)}
+        else {
+          campground.comments.push(comment)
+          campground.save()
+          res.redirect('/campgrounds/' + campground._id)
+        }
+      })
     }
   })
 })
