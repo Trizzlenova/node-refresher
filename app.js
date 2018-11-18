7cd0ed34c32a7327c23f92677225b3a26130b8bf
Trizzlenova <tdswayzee@gmail.com>
2018-11-17 19:54:00 -0800

    refactoring routes

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ const express               = require('express'),
       PassportLocalMongoose = require('passport-local-mongoose'),
       Campground            = require('./models/campground'),
       Comment               = require('./models/comment'),
-      User                  = require('./models/user')
-      seedDB                = require('./seed')
+      User                  = require('./models/user'),
+      seedDB                = require('./seed');
+
+const commentRoutes         = require('./routes/comments'),
+      campgroundRoutes      = require('./routes/campgrounds'),
+      indexRoutes            = require('./routes/index')
 
 mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true })
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,120 +33,14 @@ passport.use(new LocalStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
-app.get("/", function(req, res){
-    res.render("landing");
-});
-
-app.get('/campgrounds', function(req, res){
-    Campground.find({}, function(err, allCampgrounds){
-       if(err){
-           console.log(err);
-       } else {
-          res.render('campgrounds/index',{campgrounds:allCampgrounds});
-       }
-    });
-});
-
-app.post('/campgrounds', function(req, res){
-  let name = req.body.name
-  let image = req.body.image
-  let desc = req.body.desc
-  let newCampground = {name: name, image: image, description: desc}
-  Campground.create(newCampground, function(err, newlyCreated){
-    if(err){console.log(err)}
-    else {
-      res.redirect('campgrounds')
-    }
-  })
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
 })
 
-app.get('/campgrounds/new', function(req, res) {
-  res.render('campgrounds/new')
-})
-
-app.get('/campgrounds/:id', function(req, res) {
-  Campground.findById(req.params.id).populate('comments').exec(function(err, foundCampground){
-    if(err){console.log(err)}
-    else {
-      console.log(foundCampground)
-      res.render('campgrounds/show', {campground:foundCampground})
-    }
-  })
-})
-
-// COMMENT ROUTES
-
-app.get('/campgrounds/:id/comments/new', isLoggedIn, function(req, res) {
-  Campground.findById(req.params.id, function(err, campground) {
-    if(err){console.log(err)}
-    else {
-    res.render('comments/new', {campground: campground})
-    }
-  })
-})
-
-app.post('/campgrounds/:id/comments', isLoggedIn, function(req, res) {
-  Campground.findById(req.params.id, function(err, campground) {
-    if(err){
-      console.log(err)
-      res.redirect('/campgrounds')
-    } else {
-      Comment.create(req.body.comment, function(err, comment) {
-        if(err){console.log(err)}
-        else {
-          campground.comments.push(comment)
-          campground.save()
-          res.redirect('/campgrounds/' + campground._id)
-        }
-      })
-    }
-  })
-})
-
-// Auth Routes
-
-app.get('/register', function(req, res) {
-  res.render('register')
-})
-
-app.post('/register', function(req, res) {
-  req.body.username
-  req.body.password
-  User.register(new User({username: req.body.username}), req.body.password, function(err, user) {
-    if(err){
-      console.log(err)
-      return res.render('register')
-    }
-    passport.authenticate('local')(req, res, function() {
-      res.redirect('/campgrounds')
-    })
-  })
-})
-
-// Login Routes
-app.get('/login', function(req, res) {
-  res.render('login')
-})
-
-// middleware
-app.post('/login', passport.authenticate('local', {
-  successRedirect: '/campgrounds',
-  failureRedirect: '/login'
-  }), function(req, res) {
-})
-
-app.get('/logout', function(req, res) {
-  req.logout()
-  res.redirect('/campgrounds')
-})
-
-function isLoggedIn(req, res, next) {
-  if(req.isAuthenticated()) {
-    return next()
-  }
-  res.redirect('/login')
-}
-
+app.use('/', indexRoutes)
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/comments', commentRoutes)
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('processing yelpcamp')
